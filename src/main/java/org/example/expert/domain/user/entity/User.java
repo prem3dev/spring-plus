380f4a51abda7969f5944937f21f03b8d5048987
prem3dev <ecstasy1508@gmail.com>
2025-05-15 11:36:32 +0900

    Level.1-2 / 1-3: User Entity 수정
    - 1-2 nickName 필드 추가
    
    - 1-3 페이징 조회에서 N+1 문제를 해결하기 위해 @BatchSize 추가

--- src/main/java/org/example/expert/domain/user/entity/User.java
+++ src/main/java/org/example/expert/domain/user/entity/User.java
@@ -6,10 +6,12 @@ import lombok.NoArgsConstructor;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.common.entity.Timestamped;
 import org.example.expert.domain.user.enums.UserRole;
+import org.hibernate.annotations.BatchSize;
 
 @Getter
 @Entity
 @NoArgsConstructor
+@BatchSize(size = 10)
 @Table(name = "users")
 public class User extends Timestamped {
 
@@ -20,11 +22,14 @@ public class User extends Timestamped {
     private String password;
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
+    @Column(nullable = false, length = 20)
+    private String nickName;
 
-    public User(String email, String password, UserRole userRole) {
+    public User(String email, String password, UserRole userRole, String nickName) {
         this.email = email;
         this.password = password;
         this.userRole = userRole;
+        this.nickName = nickName;
     }
 
     private User(Long id, String email, UserRole userRole) {
