05b45dffffe579a9d984cfdb207690e582ea36cc
prem3dev <ecstasy1508@gmail.com>
2025-05-15 13:40:43 +0900

    Level.2-9 resolveArgument()에서 SecurityContextHolder로부터 인증 정보를 가져오도록 수정
    - request가 아닌 SecurityContextHolder로부터 인증 정보 검증을 거쳐 임시 인증 객체를 생성하게끔 변경

--- src/main/java/org/example/expert/config/AuthUserArgumentResolver.java
+++ src/main/java/org/example/expert/config/AuthUserArgumentResolver.java
@@ -1,12 +1,12 @@
 package org.example.expert.config;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.example.expert.domain.auth.exception.AuthException;
 import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
-import org.example.expert.domain.user.enums.UserRole;
 import org.springframework.core.MethodParameter;
 import org.springframework.lang.Nullable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -34,13 +34,22 @@ public class AuthUserArgumentResolver implements HandlerMethodArgumentResolver {
             NativeWebRequest webRequest,
             @Nullable WebDataBinderFactory binderFactory
     ) {
-        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        // JwtFilter 에서 set 한 userId, email, userRole 값을 가져옴
-        Long userId = (Long) request.getAttribute("userId");
-        String email = (String) request.getAttribute("email");
-        UserRole userRole = UserRole.of((String) request.getAttribute("userRole"));
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new AuthException("인증되지 않은 사용자입니다.");
+        }
 
-        return new AuthUser(userId, email, userRole);
+        Object principal = authentication.getPrincipal();
+
+        if (!(principal instanceof CustomUserDetails userDetails)) {
+            throw new AuthException("잘못된 인증 정보입니다.");
+        }
+
+        return new AuthUser(
+            userDetails.getId(),
+            userDetails.getEmail(),
+            userDetails.getUserRole()
+        );
     }
 }
